4eaf4f10e212b18597a97e6b8da6550c9341c90d
Hossein Arjomandi <hma.globe@gmail.com>
2020-05-31 00:52:42 +0430

    Creating and decoding the discovery message was implemented

--- src/Node/Node.java
+++ src/Node/Node.java
@@ -10,33 +10,45 @@ import java.util.Random;
 
 public class Node {
     private int port;
-    private String ip,name;
-    private ArrayList<Node>cluster;
+    private String ip, name;
+    private ArrayList<Node> cluster;
 
-    public Node( String ip, int port,String name) {
+    public Node(String ip, int port, String name) {
         this.port = port;
         this.ip = ip;
         this.cluster = new ArrayList<>();
-        this.name=name;
+        this.name = name;
 
         Random random = new Random();
         int randomInteger = random.nextInt();
 
-        port=randomInteger%65536;
+        port = randomInteger % 65536;
     }
 
-    void sendDiscovery () throws Exception
-    {
-        for (int i=0;i<cluster.size();i++)
-            sendSignal(cluster.get(i).getIp(),cluster.get(i).getPort());
+    void sendDiscovery() throws Exception {
+        String msg = createMessage();
+
+        for (int i = 0; i < cluster.size(); i++)
+            sendSignal(cluster.get(i).getIp(), cluster.get(i).getPort(), msg);
     }
 
+    String createMessage() {
+        String ans = "";
+        for (int i = 0; i < cluster.size(); i++)
+            ans = ans + cluster.get(i).getName() + "|" + cluster.get(i).getIp() + "p" + cluster.get(i).getPort() + "\n";
 
-    void sendSignal(String ip,int port) throws Exception
-    {
-       // byte[] message=(ip+"@"+port).getBytes();
-        byte[] message=("HELLO UDP").getBytes();
-        InetAddress address= InetAddress.getByName(ip);
+        return ans;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    void sendSignal(String ip, int port, String msg) throws Exception {
+        // byte[] message=(ip+"@"+port).getBytes();
+        byte[] message = msg.getBytes();
+        InetAddress address = InetAddress.getByName(ip);
         //http://www.java2s.com/Code/Java/Network-Protocol/SendoutUDPpockets.htm
         DatagramPacket packet = new DatagramPacket(message, message.length,
                 address, port);
@@ -47,8 +59,7 @@ public class Node {
     }
 
 
-    void receiveSignal()
-    {
+    void receiveSignal() {
         try {
             DatagramSocket dsocket = new DatagramSocket(port);
             //http://www.java2s.com/Code/Java/Network-Protocol/ReceiveUDPpockets.htm
@@ -58,9 +69,7 @@ public class Node {
 
             String msg = new String(buffer, 0, packet.getLength());
             System.out.println(packet.getAddress().getHostName() + ": " + msg);
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -69,32 +78,76 @@ public class Node {
         return port;
     }
 
+
+    void update(String msg) {
+        // ans=ans+cluster.get(i).getName()+"|"+cluster.get(i).getIp()+"p"+cluster.get(i).getPort()+"\n";asdasdasdasd;
+        int temp = 0, temp2 = 0;
+
+        for (int i = 0; i < msg.length(); i++)
+            if (msg.charAt(i) == '|') {
+                temp = i;
+                break;
+            }
+
+        String nodeName = msg.substring(0, temp);
+
+        for (int i = temp + 1; i < msg.length(); i++)
+            if (msg.charAt(i) == 'p') {
+                temp2 = i;
+                break;
+            }
+
+        String nodeIp = msg.substring(temp + 1, temp2);
+
+        for (int i = temp2 + 1; i < msg.length(); i++)
+            if (msg.charAt(i) == '\n') {
+                temp = i;
+                break;
+            }
+
+        int nodePort = Integer.parseInt(msg.substring(temp2 + 1, temp));
+
+        String m = msg.substring(temp + 1);
+
+        boolean currentlyExists=false;
+
+        for (int i=0;i<cluster.size();i++)
+            if(cluster.get(i).getName().equals(nodeName))
+            {
+                currentlyExists=true;
+                break;
+            }
+
+
+        if(!currentlyExists)
+            cluster.add(new Node(nodeIp,nodePort,nodeName));
+
+
+        if(m.length()>2)
+             update(m);
+
+    }
+
     public String getIp() {
         return ip;
     }
 
 
-    public void comm()
-    {
-        while(true)
-        {
+    public void comm() {
+        while (true) {
             try {
-                sendSignal("127.0.0.1", 30000);
+                sendSignal("127.0.0.1", 30000, "PIOP");
                 System.out.println("I SENT");
                 break;
-                }
-
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
     }
 
 
-
     public static void main(String[] args) {
-        Node node=new Node("127.0.0.1",30000,"N1");
+        Node node = new Node("127.0.0.1", 30000, "N1");
         node.comm();
     }
 }
